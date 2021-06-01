db4244b57cc9f8eb88b6f93ca992f57886302670
fiq-i <bakerstreet_no.221b@yahoo.com>
2021-06-01 11:04:19 +0700

    test js done

---
+++ rbprima.js
@@ -0,0 +1,28 @@
+var x = [];
+for (var i = 2; i < 30; i++) {
+    let isPrima = true;
+    for (k = 2; k < i; k++) {
+        if (i % k == 0) {
+            isPrima = false;
+            break
+        }
+    }       
+    if (isPrima)
+    x.push(i)
+}
+console.log(x);
+
+// var x = [];
+// var i = 2;
+// while (i <= 30){
+//     var j = 2
+//     while (j < i){
+//         if (i % j == 0){
+//             break
+//         }
+//         j++
+//     }
+//     i++
+// }
+
+// console.log(x);
