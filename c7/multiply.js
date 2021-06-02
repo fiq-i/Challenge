025937b35175a2f229d7497abccd3d4134cb8ed5
fiq-i <bakerstreet_no.221b@yahoo.com>
2021-06-02 18:08:40 +0700

    7th task is done

---
+++ c7/multiply.js
@@ -0,0 +1,19 @@
+function weirdMultiply(sentence) {
+    sentence = sentence.toString()
+    // write your code here
+    if (sentence.length == 1) {
+        return sentence;
+    }
+    else {
+        let numbers = 1;
+        for (let i = 0; i < sentence.length; i++) {
+            numbers *= sentence[i]
+        }
+        return weirdMultiply(numbers);
+    }
+}
+
+
+console.log(weirdMultiply(39)); //3*9=27 > 2*7=14 > 1*4 = 4
+console.log(weirdMultiply(999)); //9*9*9=729 > 7*2*9=126 > 1*2*6=12 > 1*2 =2
+console.log(weirdMultiply(3));//3
