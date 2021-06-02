e3bedfdc59e34c05cd3a550e80b2001dd26f6fe2
fiq-i <bakerstreet_no.221b@yahoo.com>
2021-06-02 16:32:49 +0700

    6th task is done

---
+++ c6/sentences.js
@@ -0,0 +1,26 @@
+function sentencesManipulation(sentence) {
+    //write your code here
+    var words = sentence.split(" ");
+    console.log(words)
+    let output = ""
+    for (let i = 0; i < words.length; i++) {
+        output += stringManipulation(words[i] + "")
+    }
+    return console.log(output)
+}
+
+function stringManipulation(word) {
+    //write your code here
+    let vowel = ['a', "e", "i", "o", "u"];
+    let result = "";
+    for (let i = 0; i <= vowel.length; i++) {
+        if (word[0].toLowerCase() == vowel[i]) {
+            return (result = word + " ")
+        }
+    }
+    let res = word.substr(1, [word.length]);
+    res += word[0] + "nyo" + " "
+    return res
+}
+
+sentencesManipulation('ibu pergi ke pasar bersama aku')
