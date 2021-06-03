d611ba85bb495c89a8cea397b892ebd6bc061cb4
fiq-i <bakerstreet_no.221b@yahoo.com>
2021-06-03 16:25:53 +0700

    9th task is not done

--- c9/spiral.js
+++ c9/spiral.js
@@ -3,8 +3,7 @@ function spiral(param1) {
     let result = [];
     if (typeof param1 == 'number') {
         let arrA = [];
-        for (let i = 0; i < param1; i++) 
-        {
+        for (let i = 0; i < param1; i++) {
             let arrB = [];
             for (let j = i * param1; j < param1 * (i + 1); j++) 
             {
@@ -12,34 +11,36 @@ function spiral(param1) {
             }
             arrA.push(arrB)
         }
-    let last = arrA[0].length-1;
-    
-    for (let i=0 ; i<=last ;i++){
-        result.push(arrA[0][i]); //kanan
-    }
 
-    for (let i=1 ;i< last ;i++){
-        result.push(arrA[i][last]); //bawah
-    }
+        let last = (arrA[0].length-1);
+        for (let i = 0; i <= last; i++) 
+        {
+            result.push(arrA[0][i]) //kanan
+        }
+        for (let i = 1; i < last; i++) 
+        {
+            result.push(arrA[i][last]) //bawah
+        }
+        for (let i = last; i >= 0; i--) 
+        {
+            result.push(arrA[last][i]) //kiri
+        }
+        for (let i = last-1; i > 0; i--) 
+        {
+            result.push(arrA[i][0]) //atas
+        }
+        
+        temp = [];
+        for (let i = 1; i < last; i++) {
+            temp.push(arrA[i].splice(1,last-i))
+        }
+        result += [spiral(temp)]
 
-    for (let i=last;i>=0 ;i--){
-        result.push(arrA[last][i]); //kiri
     }
-
-    for (let i=last-1 ;i>0 ;i--){
-        result.push(arrA[i][0]); //atas
-    } 
-
-    subarray = [];
-    for (let i=1; i<last; i++){
-        subarray.push(arrA[i].splice(1,last-1));
-    }
-    result = result.concat( spiral(subarray) );
-    }
-    console.log(result)
+    return (result)
 }
 
-spiral(3)
+console.log(spiral(3))
 // spiral(6)
 // spiral(7)
 
