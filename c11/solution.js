b6bea3b15d8f6c1433b4127923c6d6d420dce030
fiq-i <bakerstreet_no.221b@yahoo.com>
2021-06-06 18:16:19 +0700

    11th task is done

--- c11/solution.js
+++ c11/solution.js
@@ -1,41 +1,40 @@
-const fs = require ("fs");
+const fs = require("fs");
 const readline = require('readline');
 
 fs.readFile('data.json', 'utf8', (err, jsonString) => {
-	if (err){ 
+	if (err) {
 		console.log("Error reading file:", err);
-		}
-let data = JSON.parse(jsonString);
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
-	prompt: 'Tebakan:'
-})
+	}
+	let data = JSON.parse(jsonString);
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+		prompt: 'Tebakan:'
+	})
 
-console.log('Selamat datang di permainan Tebak Kata, silahkan isi dengan jawaban yang benar ya!\n');
+	console.log('Selamat datang di permainan Tebak Kata, silahkan isi dengan jawaban yang benar ya!\n');
 
-let counter = 0;
+	let counter = 0;
 
-console.log(`Pertanyaan: ${data[counter].question}`);
+	console.log(`Pertanyaan: ${data[counter].question}`);
 
-rl.prompt();
-rl.on('line', (guess) => {
-	if (guess.toLowerCase() == data[counter].answer.toLowerCase() ){
-		console.log(`Selamat Anda Benar!\n`);
-		counter += 1;
-		if (counter == data.length){
-			console.log(`Hore Anda Menang!\n`);
-			rl.close();
+	rl.prompt();
+	rl.on('line', (guess) => {
+		if (guess.toLowerCase() == data[counter].answer.toLowerCase()) {
+			console.log(`Selamat Anda Benar!\n`);
+			counter += 1;
+			if (counter == data.length) {
+				console.log(`Hore Anda Menang!\n`);
+				rl.close();
 			}
-		console.log(`Pertanyaan: ${data[counter].question} `)
+			console.log(`Pertanyaan: ${data[counter].question} `)
 		}
 		else {
-		console.log(`Wkwkwkwk, Anda kurang Beruntung!\n`)
+			console.log(`Wkwkwkwk, Anda kurang Beruntung!\n`)
 		}
-	rl.prompt();
-	
-}).on('close', () => {
-	process.exit(0);
+		rl.prompt();
+
+	}).on('close', () => {
+		process.exit(0);
 	})
 })
-	
